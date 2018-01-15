aa6a78f989d105b1bba7c785e1dbff0456bc521d
hungrypan <1602944312@qq.com>
2018-01-16 01:04:05 +0800

    init project

---
+++ src/main/java/com/example/Demo.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello Demo!
+ *
+ */
+public class Demo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Demo" );
+    }
+}
